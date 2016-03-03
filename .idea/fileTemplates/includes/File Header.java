169fc43a84f3316c23811ae913ca0bf678c5bf49
saymagic <saymagic@163.com>
2016-03-03 21:59:02 +0800

    add fake properties for debug

--- .idea/fileTemplates/includes/File Header.java
+++
@@ -1,3 +0,0 @@
-/**
- * Created by saymagic on ${DATE}.
- */
--- .idea/fileTemplates/includes/File Statement.java
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) ${YEAR} saymagic <saymagic.dev@gmail.com>
- *
- * This file is a part of project ${PROJECT_NAME}
- *
- * Licensed under the Apache License, Version 2.0 (the "License”);
- */
